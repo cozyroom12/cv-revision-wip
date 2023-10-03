e71ce4e9af136dee73ae6c9034840d3adad1a240
cozyroom12 <129810821+cozyroom12@users.noreply.github.com>
2023-10-03 23:56:15 +0900

    Add files via upload

---
+++ main_temp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# page config
+st.set_page_config(page_title='Main',
+                   layout='wide',
+                   page_icon=pass)
+
+st.title('Diabetic Retinopathy Classification Temp')
+st.caption('This web is only for presenting our model')
+
+# Content
+st.markdown('<<Content>>')
